f64fc4b68ab7aea6094e53856229181aaef55b7b
sisonechka <92185059+sisonechka@users.noreply.github.com>
2021-10-13 18:14:12 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$name = 'Sofiia';
+$age = 17;
+$pi = 3.14;
+echo "Имя $name \n";
+echo "возраст $age \n";
+echo "pi = $pi \n";
+$arr1 = [‘alex’, ‘vova’, ‘tolya’];
+$arr2 = [‘alex’, ‘vova’, ‘tolya’, [‘kostya’, ‘olya’]];
+$arr3 = [‘alex’, ‘vova’, ‘tolya’, [‘kostya’, ‘olya’, [‘gosha’, mila]]];
+$arr4 = [[‘alex’, ‘vova’, ‘tolya’], [‘kostya’, ‘olya’], [‘gosha’, mila]];
+print $arr1;
+print_r($arr1);
+print_r($arr1);
+print_r($arr1);
+?>
